2e0e64bace6154ad8a1fda81a2997caa91ecd9cd
aftonslone <afton@gmail.com>
2021-11-16 12:20:06 -0500

    Working on User model

---
+++ app/models/user.py
@@ -0,0 +1,41 @@
+from .db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+import datetime
+from .follow import follows
+
+
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    profile_photo = db.Column(db.String)
+    bio = db.Column(db.Text)
+    created_at = db.Column(db.Datetime, nullable=False, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.Datetime, nullable=False, default=datetime.datetime.utcnow)
+
+    followers = db.relationship('User', secondary=follows, primaryjoin=(follows.c.follower_id == id), secondaryjoin=(follows.c.followed_id == id), lazy="dynamic")
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
