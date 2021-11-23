5a487d9abdb411e71c3662cb52a188dc23bb1cce
aftonslone <afton@gmail.com>
2021-11-23 11:35:03 -0500

    Update

--- react-app/src/components/reply/ReplyComposer.js
+++ react-app/src/components/reply/ReplyComposer.js
@@ -37,7 +37,7 @@ export default function ReplyComposer({ setCheeps }) {
       <div>
         <div>
           <textarea
-            placeholder="What's happening?"
+            placeholder="Cheep your reply..."
             name="content"
             value={content}
             onChange={(e) => setContent(e.target.value)}
