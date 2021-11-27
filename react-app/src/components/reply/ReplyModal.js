6ef16d9f855ab696eee85ea34365ced895f37e61
aftonslone <slone.shane@gmail.com>
2021-11-27 16:58:16 -0500

    Update

--- react-app/src/components/reply/ReplyModal.js
+++ react-app/src/components/reply/ReplyModal.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { updateReplyModal } from "../../store/reply";
-import { ReplyModalContainer } from "../../Styles/Reply/ReplyModalContainer.style";
-import ReplyCheepCard from "./ReplyCheepCard";
-import ReplyComposer from "./ReplyComposer";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { updateReplyModal } from '../../store/reply';
+import { ReplyModalContainer } from '../../Styles/Reply/ReplyModalContainer.style';
+import ReplyCheepCard from './ReplyCheepCard';
+import ReplyComposer from './ReplyComposer';
 
 export default function ReplyModal({ setCheeps, update, setUpdate }) {
   const dispatch = useDispatch();
@@ -11,7 +11,11 @@ export default function ReplyModal({ setCheeps, update, setUpdate }) {
     <ReplyModalContainer>
       <span onClick={() => dispatch(updateReplyModal(false))}>X</span>
       <ReplyCheepCard />
-      <ReplyComposer setCheeps={setCheeps}  update={update} setUpdate={setUpdate}/>
+      <ReplyComposer
+        setCheeps={setCheeps}
+        update={update}
+        setUpdate={setUpdate}
+      />
     </ReplyModalContainer>
   );
 }
