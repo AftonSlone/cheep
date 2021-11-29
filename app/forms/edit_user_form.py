663a8f28af98e8313ec4b1cee1f8fdc285491d1a
aftonslone <afton@gmail.com>
2021-11-29 13:59:09 -0500

    Update

--- app/forms/edit_user_form.py
+++ app/forms/edit_user_form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from app.validators import email_exists, username_exists
 
 
 class EditUserForm(FlaskForm):
-    name = StringField('name')
-    bio = StringField('bio')
+    name = StringField('name', Length(min=3, max=25), DataRequired())
+    bio = StringField('bio', Length(max=500))
